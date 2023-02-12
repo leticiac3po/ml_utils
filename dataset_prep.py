d2bc32ab9200b8ed4e70ecaa7f52784500230a2d
leticiac3po <62350782+leticiac3po@users.noreply.github.com>
2023-02-12 17:56:04 -0300

    Add files via upload

---
+++ dataset_prep.py
@@ -0,0 +1,271 @@
+import numpy as np
+import os, cv2
+
+# structure must be: dataset -> folders for classes
+# if it's a one class model: one_class = True and which_class = CLASS_FOLDER_NAME
+# prep(base_dir,print_=False,save_all=False,img=False,val=False,
+        # split_train=0.7,split_val=0.2,classes=2,one_class=False,save_dir=None,which_class=None)
+
+def _print_dic(data,print_):
+    if print_:
+        print('VERIFY SHAPES:')
+        for key in data.keys():
+            if 'name' in key:
+                continue
+            else:
+                print(key,data[key].shape)
+
+def _shuffle_together(files):
+    seed = np.random.randint(0, 10000)
+    for fl in files:
+        np.random.seed(seed)
+        np.random.shuffle(fl)
+
+def _read_names(base_dir,filename):
+    fl = open(os.path.join(base_dir, filename+'.txt'),'r')
+    names = []
+    for line in fl:
+        line = line.replace('\n','')
+        names.append(line)
+    return names
+
+def _save_names(base_dir,filename,names):
+    fl = open(os.path.join(base_dir, filename),'w+')
+    for name in names:
+        fl.write(name+'\n')
+    fl.close()
+
+def _is_train(one_class,class_,which_class):
+    if one_class:
+        if class_ == which_class:
+            return True
+        else:
+            return False
+    else:
+        return True
+
+def _save_dic(save_dir,data,tag='',tag_in_dic=True):
+    if tag != '':
+        tag = '_' + tag
+    if tag_in_dic:
+        dic_tag = tag
+    else:
+        dic_tag = ''
+    for key in data.keys():
+        if 'name' in key:
+            _save_names(save_dir,key+tag+'.txt',data[key+dic_tag])
+        else:
+            np.save(os.path.join(save_dir, key+tag+'.npy'),data[key+dic_tag])
+
+def _load_filenames(base_dir,filenames,tag=''):
+    data = {}
+    new_filenames = []
+    if tag != '':
+        tag = '_' + tag
+    for filename in filenames:
+        key = filename.split('.')[0]
+        if 'names' in key:
+            data[key+str(tag)] = _read_names(base_dir,filename+str(tag))
+            new_filenames.append(filename+str(tag)+'.txt')
+        else:
+            data[key+str(tag)] = np.load(os.path.join(base_dir,filename+str(tag)+'.npy'))
+            new_filenames.append(filename+str(tag)+'.npy')
+    return data,new_filenames
+
+def _is_in_data(keys,key):
+    split = ['train_','test_','val_']
+    for part in split:
+        if (part + key) in keys:
+            return True
+    return False
+
+
+def _load_from_dir(base_dir,data={}):
+    keys = os.listdir(base_dir)
+    for key in keys:
+        if not('.' in key) or _is_in_data(data.keys(),key):
+            continue
+        if 'name' in key:
+            data[key.split('.')[0]] = _read_names(base_dir,key.replace('.txt',''))
+        else:
+            data[key.split('.')[0]] = np.load(os.path.join(base_dir,key))
+    return data,keys
+
+def _delete_filenames(base_dir,filenames=None):
+    if filenames == None:
+        filenames = os.listdir(base_dir)
+    for filename in filenames:
+        os.remove(os.path.join(base_dir,filename))
+
+def _prep_split(tag,data,train,val,split_train,split_val):
+    if tag == None:
+        fls = ['data','lbls','names']
+    else:
+        fls = ['data_'+str(tag),'lbls_'+str(tag),'names_'+str(tag)]
+
+    if train:
+        split_train = int(data[fls[0]].shape[0]*split_train)
+    if val:
+        split_val = int(data[fls[0]].shape[0]*(split_train + split_val))
+
+    if val:
+        if train:
+            split = ['train','test','val']
+        else:
+            split = ['test','val']
+    elif train:
+        split = ['train','test']
+
+    return fls,split_train,split_val, split
+
+def concat_one_folder(base_dir,print_=False,img=False,save_dir=None,tag=None):
+    if save_dir == None:
+        save_dir = base_dir
+    files = os.listdir(base_dir)
+    fl = np.load(os.path.join(base_dir, files[0]))
+    fl = np.expand_dims(fl,axis=0)
+
+    data={}
+    data['data'] = np.zeros((1,fl.shape[1]))
+    data['lbls'] = np.zeros((1,1))
+    data['names'] = []
+
+    for fl in files:
+        data['names'].append(os.path.join(base_dir, fl))
+        if img:
+            fl = cv2.imread(os.path.join(base_dir, fl))
+        else:
+            fl = np.load(os.path.join(base_dir, fl))
+        fl = np.expand_dims(fl,axis=0)
+        lbls = np.full((fl.shape[0],1),0)
+        data['data'] = np.concatenate([data['data'],fl],axis=0)
+        data['lbls'] = np.concatenate([data['lbls'],lbls],axis=0)
+
+    data['data'] = data['data'][1:]
+    data['lbls'] = data['lbls'][1:]
+
+    _shuffle_together([data['data'],data['lbls'],data['names']])
+    _save_dic(save_dir,data,tag=tag,tag_in_dic=False)
+    _print_dic(data,print_)
+    return list(data.keys())
+
+def split_one_class(base_dir,filenames,train=True,val=False,split_train=0.7,split_val=0.2,
+        save=True,save_dir=None,tag=None,print_=False):
+
+    if save_dir == None:
+        save_dir = base_dir
+    data,filenames = _load_filenames(base_dir,filenames,tag=tag)
+    fls,split_train,split_val,split = _prep_split(tag,data,train,val,split_train,split_val)
+
+    new_data = {}
+    if val:
+        if train:
+            for fl in fls:
+                new_data['train_'+fl] = data[fl][:split_train]
+                new_data['test_'+fl] = data[fl][split_train:split_val]
+                new_data['val_'+fl] = data[fl][split_val:]
+        else:
+            new_data['test_'+fl] = data[fl][:split_val]
+            new_data['val_'+fl] = data[fl][split_val:]
+    elif train:
+        for fl in fls:
+            new_data['train_'+fl] = data[fl][:split_train]
+            new_data['test_'+fl] = data[fl][split_train:]
+
+    for part in split:
+        _shuffle_together([new_data[part+'_'+fls[0]],new_data[part+'_'+fls[1]],new_data[part+'_'+fls[2]]])
+    if not save:
+        _delete_filenames(base_dir,filenames)
+    _save_dic(save_dir,new_data)
+    _print_dic(new_data,print_)
+    return list(new_data.keys())
+
+def _prep_concat(val,one_class,data,base_dir,one_class_dir):
+    if val:
+        split = ['train','test','val']
+    else:
+        split = ['train','test']
+
+    if one_class and not(val):
+        if one_class_dir == None:
+            one_class_dir = base_dir
+        else:
+            _load_from_dir(one_class_dir,data)
+        keys = list(data.keys())
+        for key in keys:
+            if 'train' in key or 'test' in key or 'val' in key:
+                continue
+            data['test_'+key] = data[key]
+            data.pop(key)
+
+    return split
+
+def concat_classes_after_split(base_dir,class_names,val=False,save=True,save_dir=None,one_class=False,which_class=None,one_class_dir=None,print_=False):
+    
+    if save_dir == None:
+        save_dir = base_dir
+        
+    data,filenames = _load_from_dir(base_dir)
+    split = _prep_concat(val,one_class,data,base_dir,one_class_dir)
+
+    new_data = {}
+    train = True
+    for i,class_ in enumerate(class_names):
+        if i == 0:
+            for part in split:
+                if one_class and class_ != which_class and part == 'train':
+                    train = False
+                    continue
+                new_data[part+'_data'] = data[part+'_data_'+str(class_)]
+                new_data[part+'_lbls'] = data[part+'_lbls_'+str(class_)]
+                new_data[part+'_names'] = data[part+'_names_'+str(class_)]
+        else:
+            for part in split:
+                if one_class and class_ != which_class and part == 'train':
+                    continue
+                if train == False:
+                    train = True
+                    new_data[part+'_data'] = data[part+'_data_'+str(class_)]
+                    new_data[part+'_lbls'] = data[part+'_lbls_'+str(class_)]
+                    new_data[part+'_names'] = data[part+'_names_'+str(class_)]
+                else:
+                    new_data[part+'_data'] = np.concatenate([new_data[part+'_data'], data[part+'_data_'+str(class_)]],axis=0)
+                    new_data[part+'_lbls'] = np.concatenate([new_data[part+'_lbls'], data[part+'_lbls_'+str(class_)]],axis=0)
+                    new_data[part+'_names'] = new_data[part+'_names']+data[part+'_names_'+str(class_)]
+    
+    for part in split:
+        _shuffle_together([new_data[part+'_data'],new_data[part+'_lbls'],new_data[part+'_names']])
+    if not save:
+        _delete_filenames(base_dir,filenames)
+    _save_dic(save_dir,new_data)
+    _print_dic(new_data,print_)
+    return list(new_data.keys())
+
+def _make_save_dirs(base_dir,save_dir,save_all):
+    if save_dir == None:
+        save_dir = base_dir
+    if save_all:
+        save_one = os.path.join(save_dir,'class_files')
+        save_two = os.path.join(save_dir,'class_files_split')
+        save_three = os.path.join(save_dir,'final_files')
+        if not os.path.exists(save_one):
+            os.mkdir(save_one)
+        if not os.path.exists(save_two):
+            os.mkdir(save_two)
+        if not os.path.exists(save_three):
+            os.mkdir(save_three)
+        return save_one, save_two, save_three
+    else:
+        return save_dir, save_dir, save_dir
+    
+def from_folders_to_split(base_dir,print_=False,save_all=False,img=False,val=False,split_train=0.7,
+        split_val=0.2,one_class=False,save_dir=None,which_class=None):
+    save_one, save_two, save_three = _make_save_dirs(base_dir,save_dir,save_all)
+    class_names = os.listdir(base_dir)
+    for class_name in class_names:
+        class_dir = os.path.join(base_dir,class_name)
+        saved_files = concat_one_folder(class_dir,print_,img,tag=class_name,save_dir=save_one)
+        train = _is_train(one_class,class_name,which_class)
+        if train or val:
+            split_one_class(save_one,saved_files,train,val,split_train,split_val,save_all,save_two,class_name,print_)
+    return concat_classes_after_split(save_two,class_names,val,save_all,save_three,one_class,which_class,save_one,print_)
---
+++ metrics.py
@@ -0,0 +1,178 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import cohen_kappa_score, roc_curve, auc, confusion_matrix, accuracy_score
+import numpy as np
+
+def _make_roc(y_true,Y_pred,metrics,roc_curve,which_output=0,print_=False):
+	if roc_curve:
+		if Y_pred.shape==(Y_pred.shape[0],2):
+			if which_output == 0:
+				fpr, tpr, thresholds = roc_curve(y_true, Y_pred[:,1], pos_label=1)
+			else:
+				fpr, tpr, thresholds = roc_curve(y_true, Y_pred[:1,], pos_label=1)
+		else:
+			fpr, tpr, thresholds = roc_curve(y_true, Y_pred, pos_label=1)
+		valor_auc = auc(fpr, tpr)
+		metrics["auc"] = valor_auc
+		metrics["thresholds"] = thresholds
+		plt.figure()
+		plt.plot(fpr, tpr, label='ROC curve (area = %0.4f) ' % (valor_auc))
+		plt.plot([0, 1], [0, 1], 'k--')
+		plt.xlim([0.0, 1.0])
+		plt.ylim([0.0, 1.05])
+		plt.xlabel('False Positive Rate')
+		plt.ylabel('True Positive Rate')
+		plt.title('Receiver Operating Characteristic Curve')
+		plt.legend(loc="lower right")
+		plt.close()
+		metrics['rocc'] = plt
+		if print_:
+			print('-- ROC Curve OK')
+	return metrics
+
+def _matrix_info(matrix,complete,acc,metrics,print_=False):
+	if complete and acc != 1:
+		TP = float(matrix[0][0])
+		FP = float(matrix[0][1])
+		FN = float(matrix[1][0])
+		TN = float(matrix[1][1])
+
+		if (TP+FN)>0:
+			sensitivity = TP/(TP+FN)
+		else:
+			sensitivity = 0
+		if (TN+FP)>0:
+			specificity = TN/(TN+FP)
+		else:
+			specificity = 0
+
+		if (TP+FP)>0:
+			precision = TP/(TP+FP)
+		else:
+			precision = (TP+FP)
+
+		if (sensitivity > 0) and (precision > 0):
+			f1 = 2/((sensitivity**-1)+(precision**-1))
+		else:
+			f1 = 0
+
+		metrics["sensitivity"] = sensitivity
+		metrics["specificity"] = specificity
+		metrics["f1"] = f1
+		metrics["precision"] = precision
+
+		if print_:
+			print('-- Confusion Matrix OK')
+
+	return metrics
+
+def _classify(Y_pred,threshold,smaller_than=True,which_output=0,print_=False):
+
+	if smaller_than:
+		if Y_pred.shape==(Y_pred.shape[0],2):
+			if which_output == 0:
+				y_pred = (Y_pred[:,1]<=threshold)*1
+			else:
+				y_pred = (Y_pred[:1,]<=threshold)*1
+		else:
+			y_pred = (Y_pred<=threshold)*1
+	else:
+		if Y_pred.shape==(Y_pred.shape[0],2):
+			if which_output == 0:
+				y_pred = (Y_pred[:,1]>=threshold)*1
+			else:
+				y_pred = (Y_pred[:1,]>=threshold)*1
+		else:
+			y_pred = (Y_pred>=threshold)*1
+
+	if print_:
+		print('-- Classified')
+	return y_pred
+
+def get_metrics(y,Y_pred,threshold=None,which_output=0,print_=False,roc_curve=False,complete=False,smaller_than=True):
+	metrics = {}
+	y_true = np.argmax(y, axis=-1)
+
+	if not (threshold is None):
+
+		y_pred = _classify(Y_pred,threshold,smaller_than,which_output,print_)
+
+		acc = accuracy_score(y_true, y_pred)
+		kappa = cohen_kappa_score(y_true, y_pred)
+		matrix = confusion_matrix(y_true, y_pred)
+		metrics["acc"] = acc
+		metrics["kappa"] = kappa
+		metrics["matrix"] = matrix
+
+		metrics = _matrix_info(matrix,complete,acc,metrics,print_)
+
+	metrics = _make_roc(y_true,Y_pred,metrics,roc_curve,which_output,print_)
+
+	return metrics
+
+def save_metrics(save_path,metrics,save_roc=False,print_=False):
+	fl = open(save_path + 'metrics.txt','w+')
+
+	for key in metrics.keys():
+		if 'roc' in key and save_roc:
+			metrics[key].savefig(save_path + 'roc_curve')
+		else:
+			fl.write(key+' = ' + str(metrics[key]).replace('\n','') + '\n')
+		if print_:
+			if 'matrix' in key:
+				print('-- Confusion Matrix saved')
+			elif 'roc' in key:
+				print('-- ROC Curve saved')
+			elif 'acc' in key:
+				if metrics[key] == 1:
+					print('PROBLEM: ACCURACY EQUALS 1')
+				elif metrics[key] == 0:
+					print('PROBLEM: ACCURACY EQUALS 0')
+				else:
+					print('-- Accuracy OK')
+			elif 'auc' in key:
+				if metrics[key] == 1:
+					print('PROBLEM: AREA UNDER CURVE EQUALS 1')
+				elif metrics[key] == 0:
+					print('PROBLEM: AREA UNDER CURVE EQUALS 0')
+				else:
+					print('-- Area Under Curve OK')
+		
+	fl.close()
+
+def save_mult_metrics(save_path,metrics_list,flags,save_roc=False,name_key='tag',print_=False):
+	fl = open(save_path + 'metrics_list.txt','w+')
+
+	is_name_key = False
+	for metrics in metrics_list:
+		for key in metrics.keys():
+			if key == name_key:
+				is_name_key = True
+				continue
+			fl.write(key+',')
+		if is_name_key:
+			fl.write(name_key+'\n')
+		else:
+			if print_:
+				print('''WARNING: there is no model identifier, 
+				include a unique "name_key" in the flags dictionary 
+				for each model in order to know the metrics they are associated with''')
+			fl.write('\n')
+		break
+
+	for metrics in metrics_list:
+		for key in metrics.keys():
+			if key == name_key:
+				continue
+			if 'roc' in key and save_roc:
+				metrics[key].savefig(save_path + 'roc_curve' + metrics[name_key])
+			else:
+				fl.write(str(metrics[key]).replace('\n','') + ',')
+		if metrics[name_key] == None:
+			fl.write('\n')
+			if print_:
+				if is_name_key:
+					print('WARNING: model without identifier')
+		else:
+			fl.write(metrics[name_key]+'\n')
+
+	fl.close()
